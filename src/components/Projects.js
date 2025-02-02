9d335079719d9fe32e443972f96eb2f123811c06
Sharon Shaji <sharonshaji583@gmail.com>
2025-02-02 22:26:13 +0530

    logo2

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,3 +1,4 @@
+//import { ProjectCard } from "./ProjectCard";
 import { Container, Row, Col } from "react-bootstrap";
 import projImg1 from "../assets/img/project-img1.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
